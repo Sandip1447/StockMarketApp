a1240e7f5f0e8feed1a35055d8a947fe51182b07
Sandip Rode <16761010+Sandip1447@users.noreply.github.com>
2022-11-04 12:01:10 +0530

    Added HttpLoggingInterceptor log

--- app/src/main/java/com/freecodecloud/stockmarketapp/di/AppModule.kt
+++ app/src/main/java/com/freecodecloud/stockmarketapp/di/AppModule.kt
@@ -9,9 +9,13 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.Protocol
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -20,12 +24,43 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideStockApi(): StockApi {
+    fun provideOkHttpClient(): OkHttpClient {
+
+        val interceptor = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+        } else {
+            interceptor.level = HttpLoggingInterceptor.Level.NONE
+        }
+        val httpBuilder = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(50, TimeUnit.SECONDS)
+
+        return httpBuilder
+            .protocols(mutableListOf(Protocol.HTTP_1_1))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        client: OkHttpClient
+    ): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create())
+            .client(client)
             .build()
-            .create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideStockApi(
+        retrofit: Retrofit
+    ): StockApi {
+        return retrofit.create()
     }
 
     @Provides
